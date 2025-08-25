7f66ebe687350ab4ba9c3b0711cecbd4a4e3ac9a
Swarnadeep Paul <swarnadeep9954@gmail.com>
2025-08-25 16:47:03 +0530

    Remove unistd.h form main.c and all related code

--- main.c
+++ main.c
@@ -15,22 +15,6 @@ int main(){
     printf("This is a list of all C23 standard header files!\n");
     printf("This is bugfix branch\n");
     
-    printf("Demonstrating usleep(500000): Sleeping for 0.5 seconds...\n");
-    usleep(500000); // Sleep for 500,000 microseconds (0.5 seconds)
-    printf("Woke up after 0.5 seconds!\n");
-    printf("Demonstrating sleep(1): Sleeping for 1 second...\n");
-    sleep(1); // Simulate some processing time
-    //Here is the list of all functions of unistd.h
-    printf("1. sleep(seconds): Suspend execution for a specified time.\n");
-    printf("2. usleep(microseconds): Suspend execution for a specified time in microseconds.\n");
-    printf("3. getpid(): Get the process ID of the calling process.\n");
-    printf("4. fork(): Create a new process by duplicating the calling process.\n");
-    printf("5. exec(): Replace the current process image with a new process image.\n");
-    printf("6. pipe(): Create a unidirectional data channel that can be used for interprocess communication.\n");
-    printf("7. getppid(): Get the parent process ID of the calling process.\n");
-    printf("8. ttyname(fd): Get the name of the terminal associated with a file descriptor.\n");
-    sleep(1); // Simulate some processing time
-    write(STDOUT_FILENO, "Hello, World!\n", 14);
     goodbye();
     return 0;
 }
