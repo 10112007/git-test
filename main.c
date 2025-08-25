385934f1d9d4034bf0ce6cdaa4c5ab9ccc48bece
Swarnadeep Paul <swarnadeep9954@gmail.com>
2025-08-25 16:45:28 +0530

    Remove unistd.h form main.c

--- main.c
+++ main.c
@@ -1,10 +1,10 @@
 /*
     ?[Date: 20-08-2025]
     ?This is a Code Repo to track my Git learning journey!
+    ?This repo uses Better Comments Extension for Visual Studio Code.
 */
 #include <stdio.h>
 #include <string.h>
-#include <unistd.h>
 
 const char *PROJECT_NAME = "My GIT Learning Journey";
 
@@ -16,27 +16,6 @@ int main(){
     printf("This is a list of all C23 standard header files!\n");
     printf("This is main.c and Bugfix and bugfix copy both merged\n");
 
-    printf("Demonstrating usleep(500000): Sleeping for 0.5 seconds...\n");
-    printf("This is the main branch!\n");
-    
-    usleep(500000); // Sleep for 500,000 microseconds (0.5 seconds)
-    printf("Woke up after 0.5 seconds!\n");
-    sleep(1); // Simulate some processing time
-    //Here is the list of all functions of unistd.h
-    printf("1. sleep(seconds): Suspend execution for a specified time.\n");
-    printf("2. usleep(microseconds): Suspend execution for a specified time in microseconds.\n");
-    printf("3. getpid(): Get the process ID of the calling process.\n");
-    printf("4. fork(): Create a new process by duplicating the calling process.\n");
-    printf("5. exec(): Replace the current process image with a new process image.\n");
-    printf("6. pipe(): Create a unidirectional data channel that can be used for interprocess communication.\n");
-    printf("7. getppid(): Get the parent process ID of the calling process.\n");
-    printf("8. ttyname(fd): Get the name of the terminal associated with a file descriptor.\n");
-    sleep(1); // Simulate some processing time
-    printf("9. write(fd, buf, count): Write data to a file descriptor.\n");
-    ssize_t bytes_written = write(STDOUT_FILENO, "Hello, World!\n", 14);
-    if (bytes_written == -1) {
-        perror("write");
-    }
     goodbye();
     return 0;
 }
